8858923abddf657bcd9e21a9b7f75c5e252b9d87
seokhwa <seokhwa@seokhwas-MacBook-Pro.local>
2025-06-25 10:44:57 +0900

    테스트 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Link href="/test">테스트 페이지로 이동</Link>
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -99,5 +102,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
