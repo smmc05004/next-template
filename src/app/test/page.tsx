9e45e801732e9d35ab6d82ee3412051e2bc21719
seokhwa <seokhwa@seokhwas-MacBook-Pro.local>
2025-06-25 14:41:59 +0900

    env 설정 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,11 @@
+const API_DOMAIN = process.env.NEXT_PUBLIC_API_URL;
+
 const TestPage = () => {
   return (
     <div>
       <h1>Test Page</h1>
       <p>This is a test page.</p>
+      <div>env 설정값{API_DOMAIN}</div>
     </div>
   );
 };
